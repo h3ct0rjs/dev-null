2fa2602e3a6c65b92a5efef104b5063942e90527
Hector F. Jimenez <heticor915@users.noreply.github.com>
2016-01-19 10:23:30 -0500

    Create 1002.cpp

---
+++ 1MonthofC++/URI/1002.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+#define PI 3.14159
+ 
+int main(void){
+    double R,A;
+    scanf("%lf",&R);
+    A=PI*R*R;
+    printf("A=%.4f\n",A);
+    return 0;   
+}
+/*
+feedback:hfjimeez@utp.edu.co
+URI Online Judge | 1002
+Area of a Circle:
+
+The area of the circle is defined as A = π . R2, having π as 3.14159.
+Calculate the area using the formula given in the problem description.
+
+Input
+Read the variable R (double precision), that is the radius of the circle.
+
+Output
+Print the variable A, rounded to four decimal digits.
+*/
